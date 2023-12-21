fb2d738bf2c01bcbba16f333a3f1456878e8af05
phanphan62 <nguyenyen69000@gmail.com>
2023-12-21 09:44:17 +0700

    check test

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,5 @@
 const db = require('../connection');
-
+const jwt = require('jsonwebtoken');
 exports.getNewProduct = (req, res) => {
 
     db.query('CALL getAllNewProduct', (err, results) => {
@@ -87,3 +87,94 @@ exports.listImg = (req, res) => {
         }
     });
 };
+exports.login = (req, res) => {
+    const { TaiKhoan, MatKhau } = req.body;
+    if (!TaiKhoan || !MatKhau) {
+        return res.status(400).json({ message: 'Vui lòng cung cấp tài khoản và mật khẩu' });
+    }
+    db.query(
+        'SELECT * FROM taikhoan WHERE TaiKhoan = ?', [TaiKhoan.trim()],
+        (err, results) => {
+            if (err) {
+                res.status(500).json({ message: 'Lỗi đăng nhập', error: err });
+            } else {
+                if (results.length === 0) {
+                    res.json({ message: 'Tài khoản không tồn tại' });
+                } else {
+                    const hashedPassword = results[0].MatKhau;
+                    const hashedRole = results[0].LoaiQuyen;
+                    // console.log(hashedRole);
+                    // bcrypt.compare(MatKhau, hashedPassword, (bcryptErr, isMatch) => {
+                    if (MatKhau == hashedPassword) {
+                        if (hashedRole === 'admin' || hashedRole === 'staff') {
+                            res.status(200).json({ message: 'Đăng nhập thành công chào admin, nhân viên' });
+                        } else {
+                            res.status(200).json({ message: 'Đăng nhập thành công chào khách hàng' });
+                        }
+                    } else {
+                        res.status(401).json({ message: 'Mật khẩu không chính xác' });
+                    }
+                    // });
+                }
+            }
+        }
+    );
+};
+
+// exports.login = (req, res) => {
+//     const { TaiKhoan, MatKhau } = req.body;
+
+//     if (!TaiKhoan || !MatKhau) {
+//         return res.status(400).json({ message: 'Vui lòng cung cấp tài khoản và mật khẩu' });
+//     }
+
+//     db.query(
+//         'SELECT * FROM taikhoan WHERE TaiKhoan = ?', [TaiKhoan.trim()],
+//         (err, results) => {
+//             if (err) {
+//                 res.status(500).json({ message: 'Lỗi đăng nhập', error: err });
+//             } else {
+//                 if (results.length === 0) {
+//                     res.status(401).json({ message: 'Tài khoản không tồn tại' });
+//                 } else {
+//                     const hashedPassword = results[0].MatKhau;
+//                     const hashedRole = results[0].LoaiQuyen;
+//                     console.log(hashedRole);
+//                     bcrypt.compare(MatKhau, hashedPassword, (bcryptErr, isMatch) => {
+//                         if (isMatch) {
+//                             const token = jwt.sign({ TaiKhoan, LoaiQuyen: hashedRole }, 'your_secret_key', {
+//                                 expiresIn: '1h', // Thời gian sống của token, có thể thay đổi
+//                             });
+
+//                             if (hashedRole === 'admin') {
+//                                 res.status(200).json({ message: 'Đăng nhập thành công chào admin', token });
+//                             } else {
+//                                 res.status(200).json({ message: 'Đăng nhập thành công chào nhân viên', token });
+//                             }
+//                         } else {
+//                             res.status(401).json({ message: 'Mật khẩu không chính xác' });
+//                         }
+//                     });
+//                 }
+//             }
+//         }
+//     );
+// };
+// exports.verifyToken = (req, res, next) => {
+//     const token = req.headers.authorization;
+
+
+//     if (!token) {
+//         return res.status(403).json({ message: 'Không có token được cung cấp' });
+//     }
+
+//     jwt.verify(token, 'â', (err, decoded) => {
+//         if (err) {
+//             console.log(token);
+//             return res.status(401).json({ message: 'Token không hợp lệ' });
+//         }
+
+//         req.user = decoded;
+//         next();
+//     });
+// };
